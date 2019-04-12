84d839e30be42bc5048d7db2dcc1c3d643d8e03a
Garrett Moore <mooreg2@seattleu.edu>
2019-04-11 22:08:59 -0700

    push to heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
 import VideoDetail from './components/video_detail'
-const API_KEY = `${process.env.REACT_APP_API_KEY}`
+const API_KEY = 'AIzaSyDNwz6UBvLSRjcJghGFMMNuBs-U7WJ_QgI'//`${process.env.REACT_APP_API_KEY}`
 
 //Create new component
 //Component produces jsx/html
